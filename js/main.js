b18dccbe9318c0bf6d7c9a05102c819435c190db
Jon Roundy <jonroundyfs@gmail.com>
2012-10-25 17:51:11 -0500

    Forms...
    Almost done with form...

--- js/main.js
+++ js/main.js
@@ -46,3 +46,26 @@ $(function(){
     });//Slide Menu
 	
 });
+
+$('#thisPageID').live('pagecreate', function(event) {
+  // Default picker value of Jan 1, 2012
+  var defaultPickerValue = [2012, 0, 1];
+    
+  // Make it a date
+  var presetDate = new Date(defaultPickerValue[0], defaultPickerValue[1], defaultPickerValue[2], 0, 0, 0, 0);
+    
+  // Get Today
+  var todaysDate = new Date(); 
+    
+  // Length of 1 Day
+  var lengthOfDay = 24 * 60 * 60 * 1000; 
+    
+  // Get the difference
+  var diff = parseInt((((presetDate.getTime() - todaysDate.getTime()) / lengthOfDay)+1)*-1,10); 
+    
+  // Set the origin date
+  $('#mydate').data('datebox').options.defaultPickerValue = defaultPickerValue;
+    
+  // Set minDays to disallow anything earlier
+  $('#mydate').data('datebox').options.minDays = diff; 
+});
