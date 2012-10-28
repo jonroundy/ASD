9bd9907e42da48c6d20802586d267079e8205180
Jon Roundy <jonroundyfs@gmail.com>
2012-10-27 22:40:12 -0500

    Back to the grind
    Finally making some headway took away all of the old jumbled JS
    
    Going back over resources on jquery making sense of how to properlly
    use .submit. Now if this fever would go away I could stay focused

--- js/main.js
+++ js/main.js
@@ -45,8 +45,6 @@ $(function(){
             $(p).addClass('active');
         }
     });//Slide Menu
-	
-	
 });
 
 $('#home').on('pageinit', function(){
@@ -55,66 +53,9 @@ $('#home').on('pageinit', function(){
 
 $('#addWorkorder').on('pageinit', function(){
 	
-	/*	//delete $.validator.methods.date;
-		var myform = $('#woForm'),
-			errorslink= $('#woerrorslink')
-		;
-		
-		myform.validate({
-			invalidHandler: function(form, validator){
-				errorslink.click();
-				var html = '';
-				for(var key in validator.submitted){
-					var label = $('label[for^="'+ key +'"]').not('[generated]');
-					var legend = label.closest('fieldset').find('ui-controlgroup-label');
-					var fieldName = legend.length ? legend.text() : label.text();
-					html += '<li>'+ fieldName +'</li>';
-					var cleanString = html.replace(/[:]/g, ""); //Removes all instances of
-				};
-				$("#workOrderErrors ul").html(cleanString, html)
-				//$("#taskFormErrors p").blink();
-		
-			},
-			submitHandler: function() {
-				var data = myform.serializeArray();
-				storeData(this.key);
-			}
-});//End of myform.
-*/
-}); // addWorkorder end
+$('form').submit(function() {
+  alert('Handler for .submit() called.');
+  return false;
+});
 
-//The functions below can go inside or outside the pageinit function for the page in which it is needed.
-
-var storeData = function(key){
-			if(!key){
-			var id					= Math.floor(Math.random()*100000001);
-		}else{
-			//Set the id to the existing key we're editing so that it will save over the data.
-			//The key is the same ky that's been passed along from the editSubmit event handler
-			//To the validate function, and then passed here, into the storeData function.
-			id = key;
-		}
-			//Gather up all our form field values and store in an object.
-			//Object properties are going to contain array with the form label and input value
-			getCheckboxVault();
-		var item					= {};
-			item.cats				= ["OEM List: ", $("#oemList").value];
-			item.devicelst			= ["Device List: ", $("#deviceList").value];
-			item.serial		    	= ["Serial: ", $("#serial").value];
-			item.cname		    	= ["Customer Name: ", $("#custName").value];
-			item.address		    = ["Street Address: ", $("#address").value];
-			item.city			    = ["City: ", $("#city").value];
-			item.state 			    = ["State: ", $("#stateList").value];
-			item.zipcode		    = ["Zipcode: ", $("#zipcode").value];
-			item.date				= ["Date: ", $("date").value];
-			item.urgent				= ["Urgent: ", urgentValue];
-			item.esttime			= ["Estimated Time.", $("esttime").value];
-			item.textbox			= ["Notes: ", $("textbox").value];
-			//Save data into Local Storage: Use Stringify to convert our object to a string. Local storage only stores strings.
-			//Save form elements into LS
-			localStorage.setItem(id, JSON.stringify(item));
-			console.log(storeData);
-			alert("Task Saved!");
-		loadPage();
-		
-	};
+}); // End of addWorkorder
