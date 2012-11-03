ae54cf7c7b40482f1d60a39ec7c5221fd9ab2821
Jon Roundy <jonroundyfs@gmail.com>
2012-11-03 03:22:36 -0500

    created csv data and popup
    Created csv data file and a popup on #search load. To choose which data
    type you would like to inject into the search page.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $('#home').on('pageinit', function(){
 });
 
 $('#addWorkOrder').on('pageinit', function() {
-	console.log("maybe2");
+	
 	$('#workOrderForm').validate( {
 		invalidHandler: function (form, validator) {
 			$("#formErrors ul").empty();
@@ -25,7 +25,7 @@ $('#addWorkOrder').on('pageinit', function() {
 			date: { 		required: 'Date is required.' }
 			},
 			errorPlacement: function (error, element) {
-			console.log('hello');
+			
                 $('<li>'+ error.text() +'</li>').appendTo($("#formErrors ul"));
                 
                 //$("#formErrors").dialog('refresh');
@@ -83,84 +83,11 @@ var storeData = function(key){
 		//alert(item);
 		
 }; //End of storeData.
-/*
-var getData = function(data){
-				if(localStorage.length === 0){
-			var autoFill = confirm("There are no task's to display. So default data was added.");
-			if(autoFill === true){
-			//autoFillData();
-		}
-	}
-	var makeDiv = $("#datafield");
-	var makeList = $("#dfId");
-	//makeList.setAttribute("style", "list-style:none; padding-left:2px;"); // Style Rules for ul
-	//makeDiv.appendChild(makeList);	
-	for(var i=0, len=localStorage.length; i<len; i++){
-		var makeLi = $("<li>");
-		var makeLi = $("<li>");
-		var linksLi = $("<li>");
-		// makeList.appendChild(makeLi); Delete me after testing
-		
-		$("#dfId").append('<li></li>');
-		
-		var key = localStorage.key(i);
-		var value = localStorage.getItem(key);
-		var obj = JSON.parse(value);
-		
-		var makeSubList = $("<li>");
-		//makeSubList.setAttribute("style", "list-style:none; padding-left:2px;");
-		
-		//makeLi.appendChild(makeSubList);
-		$('<li>' + '</li>')
-		
-		getImage(obj.cats[1], makeSubList);
-		for (var n in obj){
-			var makeSubLi = $("<li>");
-			//makeSubList.appendChild(makeSubLi);
-			var optSubText = obj[n][0]+" "+obj[n][1];
-			//makeSubLi.innerHTML = optSubText;
-			//makeSubLi.appendChild(linksLi);
-		}
-		makeItemLinks(localStorage.key(i), linksLi);
-	}
 
-};
-*/
-/*
-var getData = function(data){
-				if(localStorage.length === 0){
-			var autoFill = confirm("There are no task's to display. So default data was added.");
-			if(autoFill === true){
-			autoFillData();
-		};
-	};
-	var srch = $("#search_list");
-	//makeList.setAttribute("style", "list-style:none; padding-left:2px;"); // Style Rules for ul
-	 //$("#datafield").appendChild(makeList);	
-	for(var i=0, len=localStorage.length; i<len; i++){
-		
-		var key = localStorage.key(i);
-		
-		var value = localStorage.getItem(key);
-		
-		var obj = JSON.parse(value);
-		console.log(obj);
-		};
-	for(var n in obj) {
-			//var makeSubLi = document.createElement("li"); //!!! Created inside HTML
-			//makeSubList.appendChild(makeSubLi);
-			$('#optSubText').append(obj[n][0]+" "+obj[n][1]);
-			console.log(obj[n][0]+" "+obj[n][1]);
-			console.log(obj[n][0]+" ");
-			//makeSubLi.innerHTML = optSubText;
-			//makeSubLi.appendChild(linksLi);
-		}
-		//makeItemLinks(localStorage.key(i), linksLi);
-	getImage(obj.oem[1]);
-}; */
+//getImage(obj.oem[1]);
 
 var getImage = function(catName, obj){	
-		$('<img src="++"/>', {
+		$('<img/>', {
     src:     "images/oem/" + catName + ".png",
     title:   catName,
     'class': 'OEM_Logo', // in quotes because class is a reserved js word
@@ -190,15 +117,11 @@ var getData = function(data){
 	//$('<img src="++"/>', {src:     "images/oem/" + obj.oem + ".png",'class': 'OEM_Logo' })
 //$(function(){
 		console.log(obj.oem);
+		
 		$(
 			'<li>'+
-			//$('<img src="++"/>',{src:"images/oem/" + obj.oem + ".png",'class': 'OEM_Logo'})+
-			//'<img src="images/oem/" + obj.oem + ".png"><span clas="OEM_Logo"></span>'+
-			//$(function('<img src="++"/>', {src:     "images/oem/" + obj.oem + ".png",'class': 'OEM_Logo' })+
-			//'<img src="+"images/oem/" + obj.oem + ".png"+"/>'+
-			//$('<img src="++"/>').attr('src', "images/oem/" + $("#obj.oem").val() + "png")+
+			$('<img/>', {src: "images/oem/" + obj.oem + ".png",'class': 'OEM_Logo'})+
 			'<h3>' + "" + '</h3>'+
-			
 			'<p><span class="oem">Brand: </span>' + obj.oem + '</p>'+
 			'<p><span class="label">Device: </span>' + obj.device + '</p>'+
 			'<p><span class="label">Serial / Service Tag: </span>' + obj.serial + '</p>'+
@@ -253,9 +176,12 @@ $("#clear, #clear2").click(function() {
 });
 
 $('#search').on('pageinit', function(){
-		getData(datafield);
+		getData(datafield);	
+		//$( ".dataPopupLink" ).popup( "open" );
+		$("#dataPopupLink").click();
 });
 
+    
 $('#formErrors').on('pageinit', function(){
 
 });
